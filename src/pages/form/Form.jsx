4839d6754ec53041c34ea1f1dc9db3933c0358fc
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-04 02:31:25 +0400

    form page markup done plus css cleanup

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -1,7 +1,38 @@
-import React from "react";
-
+import cameraIcon from "../../assets/images/cameraIcon.svg";
+import styles from "./Form.module.css";
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <main className={styles["form-bg"]}>
+      <section className={styles["form-wrapper"]}>
+        <h2>Get Started</h2>
+        <p>add a photo</p>
+        <form className={styles["form-container"]}>
+          <label htmlFor="image" className={styles["form-add-image"]}>
+            <img src={cameraIcon} alt="add image Icon" />
+          </label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            style={{ display: "none" }}
+          />
+          <label htmlFor="name" className={styles["form-name-label"]}>
+            fill in your name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className={styles["form-name-input"]}
+            placeholder="your name"
+          />
+          <button type="submit" className={`${styles["home-button"]} btn`}>
+            Sign in
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default Form;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import notepadIcon from "../../assets/images/notepad.svg";
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className={styles["home-bg"]}>
       <img src={notepadIcon} alt="Icon of a notepad" />
       <h1 className={styles["home-title"]}>Get Started Today</h1>
-      <button type="button" className={styles["home-button"]}>
-        Get Started
-      </button>
+      <Link to={"/form"}>
+        <button type="button" className={`${styles["home-button"]} btn`}>
+          Get Started
+        </button>
+      </Link>
     </main>
   );
 };
