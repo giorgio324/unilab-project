af300dfb8b767610625ab3092bec228e590edfbb
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-04 00:14:31 +0400

    added home and form pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="form" element={<Form />} />
+    </Route>
+  )
+);
 const App = () => {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
