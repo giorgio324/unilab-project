250549f1d104c00f65b4c518ce28fa9274a6889d
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-06 19:16:12 +0400

    added api page

--- src/pages/api/Api.jsx
+++ src/pages/api/Api.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import styles from "./Api.module.css";
 
 const Api = () => {
-  return <div>Api</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getData = async (currentPage, postsPerPage) => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?_limit=${postsPerPage}&_page=${currentPage}`
+        );
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.log("api fetch error");
+      }
+    };
+    getData(currentPage, postsPerPage);
+  }, []);
+  return (
+    <div className={styles.container}>
+      {posts.map((post) => {
+        return (
+          <div key={post?.id} className={styles.card}>
+            <h2>{post?.title}</h2>
+            <p>userID: {post?.userId}</p>
+            <p>{post?.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Api;
