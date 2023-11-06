45c0a7f26094249ae424f7abbd6195d158a21aad
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-07 01:31:03 +0400

    added images and changed card width

--- src/pages/api/Api.jsx
+++ src/pages/api/Api.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import styles from "./Api.module.css";
+import Pagination from "../../components/pagination/Pagination";
 
 const Api = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
   const [posts, setPosts] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
     const getData = async (currentPage, postsPerPage) => {
       try {
@@ -12,25 +14,32 @@ const Api = () => {
           `https://jsonplaceholder.typicode.com/posts?_limit=${postsPerPage}&_page=${currentPage}`
         );
         const data = await response.json();
+        const totalCount = response.headers.get("X-Total-Count");
+        setTotalPages(Math.ceil(totalCount / 10));
         setPosts(data);
+        console.log(totalPages);
       } catch (error) {
         console.log("api fetch error");
       }
     };
     getData(currentPage, postsPerPage);
-  }, []);
+    console.log(posts);
+  }, [currentPage]);
   return (
-    <div className={styles.container}>
-      {posts.map((post) => {
-        return (
-          <div key={post?.id} className={styles.card}>
-            <h2>{post?.title}</h2>
-            <p>userID: {post?.userId}</p>
-            <p>{post?.body}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.container}>
+        {posts.map((post) => {
+          return (
+            <div key={post?.id} className={styles.card}>
+              <h2>{post?.title}</h2>
+              <p>userID: {post?.userId}</p>
+              <p>{post?.body}</p>
+            </div>
+          );
+        })}
+      </div>
+      <Pagination setCurrentPage={setCurrentPage} />
+    </>
   );
 };
 
