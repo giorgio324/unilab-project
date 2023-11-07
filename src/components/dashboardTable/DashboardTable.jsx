0cfcf2dd9e26eb459c42d3e7390c9f09500c89bf
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-08 02:26:50 +0400

    added generated data finished datatable and added pagination to dashboardtable

--- src/components/dashboardTable/DashboardTable.jsx
+++ src/components/dashboardTable/DashboardTable.jsx
@@ -1,46 +1,63 @@
+import { useState } from "react";
+import { userData } from "../../data/userData";
 import styles from "./DashboardTable.module.css";
+import Pagination from "../pagination/Pagination";
 
 const DashboardTable = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = userData.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(userData.length / itemsPerPage);
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.table}>
-        <div className={styles.head}>
-          <div>სტუდენტის სახელი და გვარი</div>
-          <div>სტატუსი</div>
-          <div>სქესი</div>
-          <div>ქულები</div>
-          <div>პირადი ნომერი</div>
-          <div>მეილი</div>
-          <div>მობილურის ნომერი</div>
-          <div>მისამართი</div>
-          <div>დაბადების თარიღი</div>
-        </div>
-        <div className={styles.line}></div>
-        <div className={styles.body}>
-          <div className={styles["body-item"]}>
-            <div>სტუდენტის სახელი და გვარი</div>
-            <div>სტატუსი</div>
-            <div>სქესი</div>
-            <div>ქულები</div>
-            <div>პირადი ნომერი</div>
-            <div>მეილი</div>
-            <div>მობილურის ნომერი</div>
-            <div>მისამართი</div>
-            <div>დაბადების თარიღი</div>
-          </div>
-          <div className={styles["body-item"]}>
-            <div>სტუდენტის სახელი და გვარი</div>
-            <div>სტატუსი</div>
-            <div>სქესი</div>
-            <div>ქულები</div>
-            <div>პირადი ნომერი</div>
-            <div>მეილი</div>
-            <div>მობილურის ნომერი</div>
-            <div>მისამართი</div>
-            <div>დაბადების თარიღი</div>
-          </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.container_wrapper}>
+        <table>
+          <thead>
+            <tr>
+              <th>სტუდენტის სახელი და გვარი</th>
+              <th>სტატუსი</th>
+              <th>სქესი</th>
+              <th>ქულები</th>
+              <th>პირადი ნომერი</th>
+              <th>მეილი</th>
+              <th>მობილურის ნომერი</th>
+              <th>მისამართი</th>
+              <th>დაბადების თარიღი</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentData?.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {user.name} {user.lastname}
+                  </td>
+                  <td>{user.status}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.scores}</td>
+                  <td>{user.privateId}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.address}</td>
+                  <td>{user.birthdate}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
+      <span className={styles.pagination_container}>
+        <Pagination
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+          totalPages={totalPages}
+        />
+      </span>
     </div>
   );
 };
