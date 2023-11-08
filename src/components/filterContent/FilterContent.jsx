d82d55cbc55abe944782d72848cd1994297e2f5e
giorgio324 <giorgikochuashvili2002@gmail.com>
2023-11-08 04:14:55 +0400

    added styles for the dropdown

--- src/components/filterContent/FilterContent.jsx
+++ src/components/filterContent/FilterContent.jsx
@@ -1,7 +1,50 @@
 import styles from "./FilterContent.module.css";
-
+import dropdownArrow from "../../assets/images/dropdownArrow.svg";
 const FilterContent = () => {
-  return <div className={styles["dropdown_container"]}>FilterContent</div>;
+  const filterData = [
+    {
+      label: "სტუდენტის სტატუსი",
+      checkboxes: [
+        { label: "ACTIVE", value: "active" },
+        { label: "INACTIVE", value: "inactive" },
+      ],
+    },
+    {
+      label: "სქესი",
+      checkboxes: [
+        { label: "MALE", value: "male" },
+        { label: "FEMALE", value: "female" },
+      ],
+    },
+  ];
+  return (
+    <div className={styles["dropdown_container"]}>
+      {filterData.map((filter, index) => {
+        return (
+          <div key={index}>
+            <div className={styles.dropdown}>
+              <img src={dropdownArrow} alt="dropdownArrow" />
+              <p>{filter.label}</p>
+            </div>
+            <div>
+              {filter.checkboxes.map((checkbox, index) => {
+                return (
+                  <div key={index} className={styles.checkbox}>
+                    <input
+                      type="checkbox"
+                      name={checkbox.value}
+                      id={checkbox.value}
+                    />
+                    <label htmlFor={checkbox.value}>{checkbox.label}</label>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FilterContent;
